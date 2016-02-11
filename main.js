8adda2900b3a60e86f15be19f20c92d2d2ad2c00
Austin Pryor <agpryor@ucdavis.edu>
2016-02-11 14:08:35 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+//Javascript
+
+//call to jQuery ready function
+//make sure html and css are loaded
+
+
+
+
+$(document).ready(function(){
+    
+    //console.log('ready')
+
+    $('#img1').click()
+        
+        console.log('ready')   
+    }01)
+    
+
+    $('#img1').hover(function() {
+                console.log('ready')
+                soundID.playSound();
+            });
+
+
+})
